321d6dda33dbb96aaebe4821070d10c80f3512f3
Akbarjon Tulaganov <akimtul@gmail.com>
2026-04-06 20:14:38 +0300

    fix esm build format

--- vite.config.ts
+++ vite.config.ts
@@ -25,5 +25,10 @@ export default defineConfig({
       transformMixedEsModules: true,
       ignore: (id) => id.includes("cjs/react"),
     },
+    rollupOptions: {
+      output: {
+        format: "es",
+      },
+    },
   },
 });
