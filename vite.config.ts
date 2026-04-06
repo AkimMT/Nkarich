32d50773a033e8a7fd439c9912ab17e24ca29754
Akbarjon Tulaganov <akimtul@gmail.com>
2026-04-06 18:48:53 +0300

    fix cjs react build

--- vite.config.ts
+++ vite.config.ts
@@ -23,12 +23,7 @@ export default defineConfig({
   build: {
     commonjsOptions: {
       transformMixedEsModules: true,
-    },
-    rollupOptions: {
-      external: (id) => {
-        if (id.includes("cjs/react-dom-client.production")) return true;
-        return false;
-      },
+      ignore: (id) => id.includes("cjs/react"),
     },
   },
 });
